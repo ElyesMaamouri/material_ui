e62921adb0768329f6f7ce07acfe2bea032ed7f9
ElyesMaamouri <elyesmaamouri@gmail.com>
2021-09-20 15:42:58 +0100

    Add Typography = h1 h2

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Typography } from "@mui/material";
 
 export default function Create() {
   return (
     <div>
-      Create page
+      {/* <Typography variant="h1" color="primary" align="center">
+        Hello MUI
+      </Typography> */}
+
+      {/* <Typography noWrap color="secondary">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </Typography> */}
+      <Typography variant="h6" component="h2" gutterBottom color="primary">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+      </Typography>
     </div>
-  )
+  );
 }
